26fc4fac6e5bdae37a294cfd451b34ff3a4bfbb0
Elise <elise.amblard@curie.fr>
2019-03-26 17:41:25 +0100

    Update ML_prepa_data.R

--- ML_prepa_data.R
+++ ML_prepa_data.R
@@ -2,16 +2,17 @@
 # path
 data <- "GSE99254_NSCLC.TCell.S12346.count.txt"
 path <- "/Users/elise/Desktop/Guo_lung_2018/"
-data <- read.csv2(paste0(path,data), sep=",", header=T)
+data <- read.table(paste0(path,data), header=T)
 data[1:5,1:5]
-# data is cell x gene
+data <- data[,-1]
+data <- data[!is.na(data[,1]),]
+rownames(data) <- data[,1]
+data <- data[,-1]
+# data is gene x cell
 # seurat object
 library(Seurat)
 library(Matrix)
-data <- Matrix(t(data),sparse=T)
-data[is.na(data)] <- 0
-colnames(data)
 seurat <- CreateSeuratObject(data)
 seurat <- NormalizeData(seurat)
-seurat@scale.Data <- t(apply(seurat@data,1,function(x) x-mean(x)))
+seurat@scale.data <- t(apply(seurat@data,1,function(x) x-mean(x)))
 # dim red ?
