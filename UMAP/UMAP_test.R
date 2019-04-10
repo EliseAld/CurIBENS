d8ef2f037e9fb117db9d8b7aa10ae2d1452dfd2f
Elise <elise.amblard@curie.fr>
2019-04-10 11:29:24 +0200

    update glmPCA/do UMAP

---
+++ UMAP/UMAP_test.R
@@ -0,0 +1,28 @@
+# UMAP function
+umap <- function(matrix, norm, center, transpose, n_neighbors, min_dist, n_compo) {
+  # matrix is gene x cell matrix
+  # norm, center and transpose are booleans
+  if (transpose == T) {
+    matrix <- (matrix)
+  }
+  if (norm = T) {
+    matrix <- apply(matrix,2,function(x) x/var(x))
+  }
+  if (center = T) {
+    matrix <- apply(matrix,2,function(x) x-mean(x))
+  }
+  library(umap)
+  umap.config <- umap.defaults
+  umap.config$n_neighbors <- n_neighbors
+  #umap.config$min_dist <- min_dist
+  umap.config$n_components <- n_components
+  umap <- umap(t(matrix), config = umap.config, method = "umap-learn")
+  return(umap)
+}
+
+# test the custom glmPCA function on Guo data
+matrix <- read.table("Guo_data_prePCA.txt", header=T)
+UMAP <- umap(matrix,norm=F,center=F,transpose=F, n_neighbors = 20, n_compo = 20)
+
+# Save obj
+save(UMAP,file="UMAP.Robj")
